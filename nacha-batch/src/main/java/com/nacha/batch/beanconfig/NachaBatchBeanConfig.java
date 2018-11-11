2563e1abbf5711a55ff6fb6b147b5f32989e35b8
Abhinav Nigam <abnig19@gmail.com>
2018-11-11 17:14:55 -0500

    [abnig] - Added fieldSetMapper

--- nacha-batch/src/main/java/com/nacha/batch/beanconfig/NachaBatchBeanConfig.java
+++ nacha-batch/src/main/java/com/nacha/batch/beanconfig/NachaBatchBeanConfig.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.mapping.PatternMatchingCompositeLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.LineTokenizer;
@@ -15,21 +17,14 @@ import org.springframework.context.annotation.Configuration;
 import com.nacha.batch.processor.PaymentBatchProcessor;
 import com.nacha.batch.processor.ReversalBatchProcessor;
 import com.nacha.batch.processor.classifier.BatchClassifier;
-import com.nacha.batch.reader.fieldmapper.AbstractACHBatchFieldSetMapper;
-import com.nacha.batch.reader.fieldmapper.AbstractACHTransactionFieldSetMapper;
-import com.nacha.batch.reader.fieldmapper.FileHeaderFieldSetMapper;
 import com.nacha.domain.batch.AbstractACHBatch;
 import com.nacha.domain.enums.ACHFileBatchDescription;
+import com.nacha.domain.file.FileHeader;
+import com.nacha.domain.transaction.AbstractACHTransaction;
 
 @Configuration
 @ComponentScan("com.nacha")
 public class NachaBatchBeanConfig {
-	
-	@Autowired
-	private FileHeaderFieldSetMapper fileHeaderFieldSetMapper;
-	
-	@Autowired
-	private  AbstractACHBatchFieldSetMapper paymentBatchFieldSetMapper;
 	
 	@Autowired
 	private PaymentBatchProcessor paymentBatchProcessor;
@@ -40,9 +35,6 @@ public class NachaBatchBeanConfig {
 	@Autowired
 	private BatchClassifier batchClassifier;
 	
-	@Autowired
-	private AbstractACHTransactionFieldSetMapper abstractACHTransactionFieldSetMapper;
-
 	@Bean
 	public DelimitedLineTokenizer delimitedLineTokenizer() {
 		DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
@@ -51,33 +43,65 @@ public class NachaBatchBeanConfig {
 	}
 	
 	@Bean
-	public LineTokenizer headerRecordLineTokenizer(DelimitedLineTokenizer delimitedLineTokenizer) {
+	public DelimitedLineTokenizer  headerRecordLineTokenizer(DelimitedLineTokenizer delimitedLineTokenizer) {
 		delimitedLineTokenizer.setNames("indicator,fileId,creationDate,creationTime,totalTransactionCount,totalCreditAmount,totalDebitAmount,batchCount".split(","));
 		return delimitedLineTokenizer;
 	}
 	
 	@Bean
-	public LineTokenizer batchRecordLineTokenizer(DelimitedLineTokenizer delimitedLineTokenizer) {
+	public DelimitedLineTokenizer  batchRecordLineTokenizer(DelimitedLineTokenizer delimitedLineTokenizer) {
 		delimitedLineTokenizer.setNames("indicator,achServiceClassCode,accountNumber,achStandardEntryClassCode,achFileBatchDescription,effectiveEntryDate,totalCreditAmount,totalDebitAmount,batchNumber,numberOfTransactionsInBatch".split(","));
 		return delimitedLineTokenizer;
 	}
 	
 	@Bean
-	public LineTokenizer transactionRecordLineTokenizer(DelimitedLineTokenizer delimitedLineTokenizer) {
-		delimitedLineTokenizer.setNames("");
+	public DelimitedLineTokenizer transactionRecordLineTokenizer(DelimitedLineTokenizer delimitedLineTokenizer) {
+		delimitedLineTokenizer.setNames("indicator, achTransactionCode, routingNumber, accountNumber, transactionAmount, identificationNumber, payeeName, transactionId, addenda".split(","));
 		return delimitedLineTokenizer;
 	}
 	
-	
 	@Bean
-	public Map<String, LineTokenizer> tokenizerMap(LineTokenizer delimitedLineTokenizer){
+	public Map<String, LineTokenizer> tokenizerMap(DelimitedLineTokenizer  headerRecordLineTokenizer, DelimitedLineTokenizer  batchRecordLineTokenizer, DelimitedLineTokenizer transactionRecordLineTokenizer){
 		Map<String, LineTokenizer> map = new HashMap<String, LineTokenizer>();
-		map.put("1*", delimitedLineTokenizer);
-		map.put("5*", delimitedLineTokenizer);		
-		map.put("6*", delimitedLineTokenizer);		
+		map.put("1*", headerRecordLineTokenizer);
+		map.put("5*", batchRecordLineTokenizer);		
+		map.put("6*", transactionRecordLineTokenizer);		
 		return map;
 	}
 	
+	@Bean
+	public BeanWrapperFieldSetMapper<FileHeader> fileHeaderFieldSetMapper() {
+		BeanWrapperFieldSetMapper<FileHeader> fileHeaderFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		fileHeaderFieldSetMapper.setTargetType(FileHeader.class);
+		return fileHeaderFieldSetMapper;
+	}
+	
+	@Bean
+	public BeanWrapperFieldSetMapper<AbstractACHBatch> abstractACHBatchFieldSetMapper() {
+		BeanWrapperFieldSetMapper<AbstractACHBatch> abstractACHBatchFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		abstractACHBatchFieldSetMapper.setTargetType(AbstractACHBatch.class);
+		return abstractACHBatchFieldSetMapper;
+	}
+	
+	@Bean
+	public BeanWrapperFieldSetMapper<AbstractACHTransaction> abstractACHTransactionFieldSetMapper() {
+		BeanWrapperFieldSetMapper<AbstractACHTransaction> abstractACHTransactionFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		abstractACHTransactionFieldSetMapper.setTargetType(AbstractACHTransaction.class);
+		return abstractACHTransactionFieldSetMapper;
+	}
+	
+	@Bean
+	public Map<String, FieldSetMapper<?>> fieldSetMapperMap(BeanWrapperFieldSetMapper<FileHeader> fileHeaderFieldSetMapper, 
+			BeanWrapperFieldSetMapper<AbstractACHBatch> abstractACHBatchFieldSetMapper, 
+			BeanWrapperFieldSetMapper<AbstractACHTransaction> abstractACHTransactionFieldSetMapper){
+		Map<String, FieldSetMapper<?>> map = new HashMap<String, FieldSetMapper<?>>();
+		map.put("1*", fileHeaderFieldSetMapper);
+		map.put("5*", abstractACHBatchFieldSetMapper);		
+		map.put("6*", abstractACHTransactionFieldSetMapper);		
+		return map;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Bean
 	public PatternMatchingCompositeLineMapper patternMatchingCompositeLineMapper(Map<String, LineTokenizer> tokenizerMap, Map<String, LineTokenizer> fieldSetMapperMap) {
 		PatternMatchingCompositeLineMapper patternMatchingCompositeLineMapper = new PatternMatchingCompositeLineMapper<>();
